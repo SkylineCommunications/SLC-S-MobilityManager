1493f826d780fc5602843b81f20218815e661b81
Tarik Kahrić <44073694+Crni88@users.noreply.github.com>
2025-11-11 15:09:09 +0100

    Removed DMS

--- Poly Line String To Route (2)/Poly Line String To Route (2).cs
+++ Poly Line String To Route (2)/Poly Line String To Route (2).cs
@@ -1,129 +1,124 @@
 namespace PolyLineToRoute
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using Skyline.DataMiner.Analytics.GenericInterface;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Skyline.DataMiner.Analytics.GenericInterface;
 
-    [GQIMetaData(Name = "Poly Line String To Route Tarik")]
-    public sealed class PolyLineToRoute : IGQIDataSource, IGQIOnInit, IGQIInputArguments, IGQIOnPrepareFetch
-    {
-        private readonly GQIStringArgument _lineStringArgument = new GQIStringArgument("Line String") { IsRequired = true };
+	[GQIMetaData(Name = "Poly Line String To Route Tarik")]
+	public sealed class PolyLineToRoute : IGQIDataSource, IGQIOnInit, IGQIInputArguments, IGQIOnPrepareFetch
+	{
+		private readonly GQIStringArgument _lineStringArgument = new GQIStringArgument("Line String") { IsRequired = true };
+		private string _lineString;
 
-        private GQIDMS _dms;
+		public OnInitOutputArgs OnInit(OnInitInputArgs args)
+		{
+			return default;
+		}
 
-        private string _lineString;
+		public OnPrepareFetchOutputArgs OnPrepareFetch(OnPrepareFetchInputArgs args)
+		{
+			return default;
+		}
 
-        public OnInitOutputArgs OnInit(OnInitInputArgs args)
-        {
-            _dms = args.DMS;
+		public GQIArgument[] GetInputArguments()
+		{
+			return new GQIArgument[] { _lineStringArgument };
+		}
 
-            return default;
-        }
+		public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
+		{
+			_lineString = args.GetArgumentValue(_lineStringArgument);
 
-        public OnPrepareFetchOutputArgs OnPrepareFetch(OnPrepareFetchInputArgs args)
-        {
-            return default;
-        }
+			return default;
+		}
 
-        public GQIArgument[] GetInputArguments()
-        {
-            return new GQIArgument[] { _lineStringArgument };
-        }
+		public GQIColumn[] GetColumns()
+		{
+			return new GQIColumn[]
+			{
+				new GQIStringColumn("Identifier"),
+				new GQIDoubleColumn("Source Latitude"),
+				new GQIDoubleColumn("Source Longitude"),
+				new GQIDoubleColumn("Destination Latitude"),
+				new GQIDoubleColumn("Destination Longitude"),
+			};
+		}
 
-        public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
-        {
-            _lineString = args.GetArgumentValue(_lineStringArgument);
+		public GQIPage GetNextPage(GetNextPageInputArgs args)
+		{
+			return new GQIPage(GetGeoPoints().ToArray())
+			{
+				HasNextPage = false,
+			};
+		}
 
-            return default;
-        }
+		private List<GQIRow> GetGeoPoints()
+		{
+			var rows = new List<GQIRow>();
 
-        public GQIColumn[] GetColumns()
-        {
-            return new GQIColumn[]
-            {
-                new GQIStringColumn("Identifier"),
-                new GQIDoubleColumn("Source Latitude"),
-                new GQIDoubleColumn("Source Longitude"),
-                new GQIDoubleColumn("Destination Latitude"),
-                new GQIDoubleColumn("Destination Longitude"),
-            };
-        }
+			if (string.IsNullOrEmpty(_lineString))
+			{
+				return rows;
+			}
 
-        public GQIPage GetNextPage(GetNextPageInputArgs args)
-        {
-            return new GQIPage(GetGeoPoints().ToArray())
-            {
-                HasNextPage = false,
-            };
-        }
+			var geoPoints = GeoParser.Parse(_lineString);
+			int i = 1;
 
-        private List<GQIRow> GetGeoPoints()
-        {
-            var rows = new List<GQIRow>();
+			foreach (var geoPoint in geoPoints)
+			{
+				rows.Add(new GQIRow(
+				new[]
+				{
+					new GQICell { Value = i++.ToString()},
+					new GQICell { Value = geoPoint.SourceLatitude },
+					new GQICell { Value = geoPoint.SourceLongitude },
+					new GQICell { Value = geoPoint.DestinationLatitude },
+					new GQICell { Value = geoPoint.DestinationLongitude },
+				}));
+			}
 
-            if (string.IsNullOrEmpty(_lineString))
-            {
-                return rows;
-            }
+			return rows;
+		}
+	}
 
-            var geoPoints = GeoParser.Parse(_lineString);
-            int i = 1;
+	public class GeoSegment
+	{
+		public double SourceLatitude { get; set; }
 
-            foreach (var geoPoint in geoPoints)
-            {
-                rows.Add(new GQIRow(
-                new[]
-                {
-                    new GQICell { Value = i++.ToString()},
-                    new GQICell { Value = geoPoint.SourceLatitude },
-                    new GQICell { Value = geoPoint.SourceLongitude },
-                    new GQICell { Value = geoPoint.DestinationLatitude },
-                    new GQICell { Value = geoPoint.DestinationLongitude },
-                }));
-            }
+		public double SourceLongitude { get; set; }
 
-            return rows;
-        }
-    }
+		public double DestinationLatitude { get; set; }
 
-    public class GeoSegment
-    {
-        public double SourceLatitude { get; set; }
+		public double DestinationLongitude { get; set; }
+	}
 
-        public double SourceLongitude { get; set; }
+	public class GeoParser
+	{
+		public static List<GeoSegment> Parse(string input)
+		{
+			var segments = new List<GeoSegment>();
 
-        public double DestinationLatitude { get; set; }
+			var pairs = input
+				.Split(new[] { "],[" }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(p => p.Replace("[", string.Empty).Replace("]", string.Empty))
+				.Select(p => p.Split(',')
+							  .Select(x => double.Parse(x.Trim(), System.Globalization.CultureInfo.InvariantCulture))
+							  .ToArray())
+				.ToList();
 
-        public double DestinationLongitude { get; set; }
-    }
+			for (int i = 0; i < pairs.Count - 1; i++)
+			{
+				segments.Add(new GeoSegment
+				{
+					SourceLongitude = pairs[i][0],
+					SourceLatitude = pairs[i][1],
+					DestinationLongitude = pairs[i + 1][0],
+					DestinationLatitude = pairs[i + 1][1],
+				});
+			}
 
-    public class GeoParser
-{
-    public static List<GeoSegment> Parse(string input)
-    {
-        var segments = new List<GeoSegment>();
-
-        var pairs = input
-            .Split(new[] { "],[" }, StringSplitOptions.RemoveEmptyEntries)
-            .Select(p => p.Replace("[", string.Empty).Replace("]", string.Empty))
-            .Select(p => p.Split(',')
-                          .Select(x => double.Parse(x.Trim(), System.Globalization.CultureInfo.InvariantCulture))
-                          .ToArray())
-            .ToList();
-
-        for (int i = 0; i < pairs.Count - 1; i++)
-        {
-            segments.Add(new GeoSegment
-            {
-                SourceLongitude = pairs[i][0],
-                SourceLatitude = pairs[i][1],
-                DestinationLongitude = pairs[i + 1][0],
-                DestinationLatitude = pairs[i + 1][1],
-            });
-        }
-
-        return segments;
-    }
-}
+			return segments;
+		}
+	}
 }
